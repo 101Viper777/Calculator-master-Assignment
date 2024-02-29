987f9bd3314b08b14672cb3035476b465b755e97
Abdul <127184231+101Viper777@users.noreply.github.com>
2024-03-01 10:24:21 +1100

    update

--- calc/Calculator.swift
+++ calc/Calculator.swift
@@ -1,37 +1,68 @@
-//
-//  Calculator.swift
-//  calc
-//
-//  Created by Jacktator on 31/3/20.
-//  Copyright © 2020 UTS. All rights reserved.
-//
-
 import Foundation
 
 class Calculator {
-    
-    /// For multi-step calculation, it's helpful to persist existing result
-    var currentResult = 0;
-    
-    /// Perform Addition
-    ///
-    /// - Author: Jacktator
-    /// - Parameters:
-    ///   - no1: First number
-    ///   - no2: Second number
-    /// - Returns: The addition result
-    ///
-    /// - Warning: The result may yield Int overflow.
-    /// - SeeAlso: https://developer.apple.com/documentation/swift/int/2884663-addingreportingoverflow
     func add(no1: Int, no2: Int) -> Int {
-        return no1 + no2;
+        return no1 + no2
     }
     
+    func subtract(no1: Int, no2: Int) -> Int {
+        return no1 - no2
+    }
+    func multiply(no1: Int, no2: Int) -> Int {
+        return no1 * no2
+    }
+    
+    func divide(no1: Int, no2: Int) -> Int {
+        return no1 / no2
+    }
+    func modulo(no1: Int, no2: Int) -> Int {
+        return no1 % no2
+    }
     func calculate(args: [String]) -> String {
-        // Todo: Calculate Result from the arguments. Replace dummyResult with your actual result;
-        let dummyResult = add(no1: 1, no2: 2);
+        // Ensure there's at least one number to start with
+        guard let firstArg = args.first else {
+            exit(1)
+        }
         
-        let result = String(dummyResult);
-        return(result)
+        guard let initialNumber = Int(firstArg) else {
+            exit(1)
+        }
+        var result = initialNumber
+//        print(result)
+
+        if args.count == 1{
+            return "\(initialNumber)" // pass the int version not original
+            
+        }
+//        print(initialNumber)
+        
+        // Iterate over the array, skipping the first number since we've already processed it
+        for i in stride(from: 1, to: args.count, by: 2) {
+            if i + 1 < args.count, let nextNumber = Int(args[i + 1]) {
+
+
+                switch args[i] {
+                case "+":
+                    result = add(no1: result, no2: nextNumber)
+//                    print(result)
+                case "-":
+                    result = subtract(no1: result, no2: nextNumber)
+                    
+                case "x":
+                    result = multiply(no1: result, no2: nextNumber)
+                    
+                case "/":
+                    result = divide(no1: result, no2: nextNumber)
+                    
+                case "%":
+                    result = modulo(no1: result, no2: nextNumber)
+
+                default:
+                    exit(1)                }
+            } else {
+                exit(1)            }
+        }
+        
+        return "\(result)"
     }
 }
